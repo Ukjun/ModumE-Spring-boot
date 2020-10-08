c33bffd79dfc7e783b836edf2c6ec4fe7a364c28
Dendrocopos-kr <Dendrocopos-kr@users.noreply.github.com>
2020-10-09 00:32:37 +0900

    mybatis연결을 위한 sql세션팩토리,  sql세션템플릿 구현

--- src/main/java/com/amolrang/modume/config/DatabaseCoinfiguration.java
+++ src/main/java/com/amolrang/modume/config/DatabaseCoinfiguration.java
@@ -2,7 +2,12 @@ package com.amolrang.modume.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -16,6 +21,21 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @PropertySource("classpath:/application.properties")
 public class DatabaseCoinfiguration {
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/*.xml"));
+		return sqlSessionFactoryBean.getObject();
+	}
+	
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
 	
 	@Bean
 	@ConfigurationProperties(prefix = "spring.datasource.hikari")
