0f00942207e0a73724738894e228fd1b5365434e
Dendrocopos-kr <Dendrocopos-kr@users.noreply.github.com>
2020-10-13 12:06:53 +0900

    정리, kakao twitch client id, secret

--- src/main/java/com/amolrang/modume/controller/TestController.java
+++ src/main/java/com/amolrang/modume/controller/TestController.java
@@ -1,50 +1,17 @@
 package com.amolrang.modume.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Data;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class TestController {
 	@RequestMapping(value = "/api/oauth2/code/twitch")
 	public void twitchAPI() {
-		System.out.println("twitch api oauth2 접근");
+		System.out.println("twitch api oauth2 접근 확인");
 	}
-	
-	public void ouauth2code() {
-		
-	}
-}
-
-@Data
-class TwitchAPI{
-	private String code;
-	private static final String AUTHORIZATION_URL
-    = "https://api.twitch.tv/kraken/oauth2/authorize" +
-    "?response_type=code" +
-    "&client_id=%s" +
-    "&redirect_uri=%s" +
-    "&scope=%s" +
-    "&state=%s";
-
-private static final String ACCESSTOKEN_URL
-    = "https://api.twitch.tv/kraken/oauth2/token" +
-    "&client_id=%s" +
-    "&client_secret=%s" +
-    "&grant_type=authorization_code" +
-    "&redirect_uri=%s" +
-    "&code=%s" +
-    "&state=%s";
-
-	protected TwitchAPI(){}
-	
-	private static class InstanceHolder{
-		private static final TwitchAPI INSTANCE = new TwitchAPI();
-	}
-	
-	public static TwitchAPI instance() {
-		return InstanceHolder.INSTANCE;
-	}
-
 }
