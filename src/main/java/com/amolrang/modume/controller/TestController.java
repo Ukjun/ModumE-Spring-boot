56b551f6eda43b89ddbde39dc15e3cb3c21f5306
Dendrocopos-kr <Dendrocopos-kr@users.noreply.github.com>
2020-10-13 11:08:44 +0900

    https://preamtree.tistory.com/167 restTemplate 빈생성 및 주입

--- src/main/java/com/amolrang/modume/controller/TestController.java
+++ src/main/java/com/amolrang/modume/controller/TestController.java
@@ -1,46 +1,50 @@
 package com.amolrang.modume.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.comhix.twitch.api.TwitchApi;
-import de.comhix.twitch.api.oauth.ClientInformation;
-import de.comhix.twitch.api.oauth.OAuthResponse;
-import de.comhix.twitch.api.oauth.TwitchAuthenticator;
-import de.comhix.twitch.api.oauth.TwitchAuthenticator.SecondStep;
-import io.reactivex.Observable;
+import lombok.Data;
 
 @RestController
 public class TestController {
-	String token_type;
-	// String token_URL =
-	// "https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=nb7cdnmp6y4wuqcnfh31c92oh0k8l7&redirect_uri=http://localhost:8080/access_token";
-	SecondStep TP;
-	ClientInformation CIF;
-	String clientID;
-	String secret;
-	TwitchApi tAPIApi;// = new TwitchApi(CIF, access_token);
+	@RequestMapping(value = "/api/oauth2/code/twitch")
+	public void twitchAPI() {
+		System.out.println("twitch api oauth2 접근");
+	}
+	
+	public void ouauth2code() {
+		
+	}
+}
 
-	@RequestMapping(value = "/TwitchSet")
-	@ResponseBody
-	public SecondStep getCode() {
-		clientID = "nb7cdnmp6y4wuqcnfh31c92oh0k8l7";
-		secret = "ssno1u6u6brdmi7jkbk5no0rw6j91d";
-		CIF = new ClientInformation(clientID, secret, "http://localhost:8080/");
-		TP = new TwitchAuthenticator(CIF,"http://localhost:8080/getCode").doAuth();
-		return TP;
+@Data
+class TwitchAPI{
+	private String code;
+	private static final String AUTHORIZATION_URL
+    = "https://api.twitch.tv/kraken/oauth2/authorize" +
+    "?response_type=code" +
+    "&client_id=%s" +
+    "&redirect_uri=%s" +
+    "&scope=%s" +
+    "&state=%s";
+
+private static final String ACCESSTOKEN_URL
+    = "https://api.twitch.tv/kraken/oauth2/token" +
+    "&client_id=%s" +
+    "&client_secret=%s" +
+    "&grant_type=authorization_code" +
+    "&redirect_uri=%s" +
+    "&code=%s" +
+    "&state=%s";
+
+	protected TwitchAPI(){}
+	
+	private static class InstanceHolder{
+		private static final TwitchAPI INSTANCE = new TwitchAPI();
+	}
+	
+	public static TwitchAPI instance() {
+		return InstanceHolder.INSTANCE;
 	}
 
-	@RequestMapping(value = "/getCode")
-	public Observable<OAuthResponse> getCode(@RequestParam(required = false) String code) {
-		System.out.println("get Code : "+code);
-		return TP.getOAuthToken(code);
-	}
-
-	@RequestMapping(value = "/getToken")
-	public String getToken(@RequestParam(required = true) String token) {
-		return String.format("%s", token);
-	}
 }
