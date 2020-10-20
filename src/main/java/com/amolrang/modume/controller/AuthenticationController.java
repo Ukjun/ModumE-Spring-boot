b7111f081c5025ef1e6af2374a245925b96f53bc
류욱준 <corn1107@hanmail.net>
2020-10-20 16:33:57 +0900

    코드 정리

--- src/main/java/com/amolrang/modume/controller/AuthenticationController.java
+++ src/main/java/com/amolrang/modume/controller/AuthenticationController.java
@@ -4,7 +4,7 @@ import java.security.Principal;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
